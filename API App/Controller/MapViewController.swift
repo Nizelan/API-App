4f5dad8b67670f191b77ff8ff8568ad57f60d4eb
Nizelan <saintnizelan@gmail.com>
2020-06-03 02:59:18 -0700

    location request commit

--- API App/Controller/MapViewController.swift
+++ API App/Controller/MapViewController.swift
@@ -7,21 +7,48 @@
 //
 
 import UIKit
+import CoreLocation
 
 class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManadger.requestLocation()
+        }
     }
     
-    @IBOutlet weak var cityNameLabel: UILabel! 
-    
+    @IBOutlet weak var cityNameLabel: UILabel!
     @IBAction func clouseMap() {
         dismiss(animated: true)
     }
-    
     @IBAction func searchButton(_ sender: UIButton) {
     }
     
+    var networkLocationManadger = NetworkManadger()
+    lazy var locationManadger: CLLocationManager = {
+        let lm = CLLocationManager()
+        lm.delegate = self
+        lm.desiredAccuracy = kCLLocationAccuracyKilometer
+        lm.requestWhenInUseAuthorization()
+        return lm
+    }()
 
+}
+
+
+// MARK: CLLocationManagerDelegate
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let latitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
+        
+        //networkLocationManadger.fetchAdress(closure: <#T##([Address]) -> ()#>)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
 }
